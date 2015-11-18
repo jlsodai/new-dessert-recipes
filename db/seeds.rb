8c9bf462923107d0006bdd98410f2c959b4d7a00
jlsodai <Jlsodai@gmail.com>
2015-11-18 10:57:47 +0000

    add gem seed_dump and generate seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Author.create!([
+  {name: "asldf", username: "asdhf", password: nil, email: "alsdhfas@ashda.com", bio: "hsdfasd alsdasdf", expertise: nil}
+])
+Comment.create!([
+  {name: "Maestro", email: "jlsodai@gmail.com", recipe_id: 1},
+  {name: "Fasigna", email: "fasigna@gmail.com", recipe_id: 1}
+])
+Recipe.create!([
+  {name: "Banku", image: nil, description: "Hello something great", ingredients: nil, directions: nil, ratings: nil, author_id: 2, dessert_type_id: 1},
+  {name: "Sugarcane Sauce", image: nil, description: "Something happens everytime", ingredients: nil, directions: nil, ratings: nil, author_id: 2, dessert_type_id: 1}
+])
