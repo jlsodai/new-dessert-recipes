51a97eb719079bd928369a9c7b6e14c76a6aaef5
Patience Nyarko <karlarish@gmail.com>
2015-11-18 11:37:08 +0000

    updated dessert-type and recipe indexes

--- app/controllers/dessert_types_controller.rb
+++ app/controllers/dessert_types_controller.rb
@@ -1,71 +1,71 @@
 class DessertTypesController < ApplicationController
   def index
-    @kofis = Kofi.all
+    @dessert_types = DessertType.all
   end
 
-  # GET /kofis/1
-  # GET /kofis/1.json
+  # GET /dessert_types/1
+  # GET /dessert_types/1.json
   def show
   end
 
-  # GET /kofis/new
+  # GET /dessert_types/new
   def new
-    @kofi = Kofi.new
+    @dessert_type = DessertType.new
   end
 
-  # GET /kofis/1/edit
+  # GET /dessert_types/1/edit
   def edit
   end
 
-  # POST /kofis
-  # POST /kofis.json
+  # POST /dessert_types
+  # POST /dessert_types.json
   def create
-    @kofi = Kofi.new(kofi_params)
+    @dessert_type = DessertType.new(dessert_type_params)
 
     respond_to do |format|
-      if @kofi.save
-        format.html { redirect_to @kofi, notice: 'Kofi was successfully created.' }
-        format.json { render :show, status: :created, location: @kofi }
+      if @dessert_type.save
+        format.html { redirect_to @dessert_type, notice: 'DessertType was successfully created.' }
+        format.json { render :show, status: :created, location: @dessert_type }
       else
         format.html { render :new }
-        format.json { render json: @kofi.errors, status: :unprocessable_entity }
+        format.json { render json: @dessert_type.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /kofis/1
-  # PATCH/PUT /kofis/1.json
+  # PATCH/PUT /dessert_types/1
+  # PATCH/PUT /dessert_types/1.json
   def update
     respond_to do |format|
-      if @kofi.update(kofi_params)
-        format.html { redirect_to @kofi, notice: 'Kofi was successfully updated.' }
-        format.json { render :show, status: :ok, location: @kofi }
+      if @dessert_type.update(dessert_type_params)
+        format.html { redirect_to @dessert_type, notice: 'DessertType was successfully updated.' }
+        format.json { render :show, status: :ok, location: @dessert_type }
       else
         format.html { render :edit }
-        format.json { render json: @kofi.errors, status: :unprocessable_entity }
+        format.json { render json: @dessert_type.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /kofis/1
-  # DELETE /kofis/1.json
+  # DELETE /dessert_types/1
+  # DELETE /dessert_types/1.json
   def destroy
-    @kofi.destroy
+    @dessert_type.destroy
     respond_to do |format|
-      format.html { redirect_to kofis_url, notice: 'Kofi was successfully destroyed.' }
+      format.html { redirect_to dessert_types_url, notice: 'DessertType was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
 
   private
     # Use callbacks to share common setup or constraints between actions.
-    def set_kofi
-      @kofi = Kofi.find(params[:id])
+    def set_dessert_type
+      @dessert_type = DessertType.find(params[:id])
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.
-    def kofi_params
-      params.require(:kofi).permit(:name)
+    def dessert_type_params
+      params.require(:dessert_type).permit(:name)
     end
 end
 
