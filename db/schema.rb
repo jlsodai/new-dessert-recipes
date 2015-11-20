24750a2bf9a9f373a7c36a74abed1bc730cc798b
jlsodai <Jlsodai@gmail.com>
2015-11-20 11:11:46 +0000

    run migration to add image support for dessert types and authors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151119195319) do
+ActiveRecord::Schema.define(version: 20151120071050) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -20,8 +20,12 @@ ActiveRecord::Schema.define(version: 20151119195319) do
     t.string   "email"
     t.text     "bio"
     t.boolean  "expertise"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "comments", force: :cascade do |t|
@@ -36,9 +40,12 @@ ActiveRecord::Schema.define(version: 20151119195319) do
 
   create_table "dessert_types", force: :cascade do |t|
     t.string   "type"
-    t.string   "image"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "recipes", force: :cascade do |t|
