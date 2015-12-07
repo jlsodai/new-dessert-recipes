c83fa92bf8fd547fc4476c0f1b6a34b47222613f
Julius A. Odai <jlsodai@gmail.com>
2015-12-07 20:45:49 +0000

    initial styling

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
   get    'login'   => 'sessions#new'
   post   'login'   => 'sessions#create'
   delete 'logout'  => 'sessions#destroy'
-  root 'recipes#index'
+  root 'recipes#home'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
